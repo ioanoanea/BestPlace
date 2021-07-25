2b5460d1b70838cdb0ddfafdfa7a6a11b756c83c
ioanoanea <ioanoanea8@gmail.com>
2021-07-25 23:28:20 +0300

    location repository

--- app/src/main/java/com/bestplace/data/repository/FirebaseRepository.kt
+++ app/src/main/java/com/bestplace/data/repository/FirebaseRepository.kt
@@ -3,10 +3,22 @@ package com.bestplace.data.repository
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.concurrent.Executor
 
-abstract class FirebaseRepository<T>(
-    private val collectionName: String
+abstract class FirebaseRepository(
+    private val collectionName: String,
+    val executor: Executor
 ) {
+
+    /**
+     * Query result class
+     * @param out R
+     */
+    sealed class Result<out R> {
+        data class Success<out T>(val data: T? = null): Result<T>()
+        data class Error(val exception: Exception): Result<Nothing>()
+    }
+
     /**
      * Get Firestore collection reference
      * @return CollectionReference
--- app/src/main/java/com/bestplace/ui/activities/HomeScreenActivity.kt
+++ app/src/main/java/com/bestplace/ui/activities/HomeScreenActivity.kt
@@ -3,8 +3,15 @@ package com.bestplace.ui.activities
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bestplace.R
+import com.bestplace.data.model.Location
+import com.bestplace.data.repository.FirebaseRepository
+import com.bestplace.data.repository.LocationRepository
 import com.bestplace.ui.fragments.CategoriesFragment
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class HomeScreenActivity : AppCompatActivity() {
 
@@ -13,6 +20,23 @@ class HomeScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
         supportFragmentManager.beginTransaction().add(R.id.container, CategoriesFragment()).commit()
+
+        val executorService: ExecutorService = Executors.newFixedThreadPool(4)
+
+        val locationRepository = LocationRepository(executorService)
+
+        locationRepository.getById("2") { result ->
+            when(result) {
+                is FirebaseRepository.Result.Success<Location> ->
+                    if (result.data != null){
+                        Toast.makeText(this, result.data.latitude.toString(), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Not Found", Toast.LENGTH_SHORT).show()
+                    }
+                else ->
+                    Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
